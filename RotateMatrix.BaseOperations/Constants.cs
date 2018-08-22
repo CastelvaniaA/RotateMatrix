9fe1c28edd305791475a30e88871236cea05f3c3
Alex <grudininalexei@mail.ru>
2018-08-22 20:01:18 +0300

    First commit

--- RotateMatrix.BaseOperations/Constants.cs
+++ RotateMatrix.BaseOperations/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RotateMatrix.BaseOperations
+﻿namespace RotateMatrix.BaseOperations
 {
     public enum RotateDirection
     {
